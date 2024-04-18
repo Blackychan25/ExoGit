965ccdbfd8347ba61ff6ab2c95123f95eb973d60
InesPasDinspi <inesnoureddinepro@gmail.com>
2024-04-18 10:28:10 +0200

    personne Ines dans le main

---
+++ mavenTestGit/src/main/java/mavenTestGit/Personne.java
@@ -0,0 +1,12 @@
+package mavenTestGit;
+
+public class Personne {
+	private String coucou;
+	
+	public void m1() {
+		System.out.println("m1i");
+	}
+	public void m2() {
+		System.out.println("m2i");
+	}
+}
